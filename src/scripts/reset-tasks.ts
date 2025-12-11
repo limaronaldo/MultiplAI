4201caae7aee661b17eac07f1387d276f2973406
Ronaldo Martins <ron@ldinho.com.br>
2025-12-11 01:12:17 -0300

    chore: move reset-tasks to src/scripts

--- src/scripts/reset-tasks.ts
+++ src/scripts/reset-tasks.ts
@@ -1,20 +1,20 @@
-import { getDb } from "./src/integrations/db";
+import { getDb } from "../integrations/db";
 
 async function resetTasks() {
   const sql = getDb();
-  
+
   const result = await sql`
-    UPDATE tasks 
-    SET status = 'NEW', 
-        attempt_count = 0, 
-        last_error = NULL, 
-        current_diff = NULL, 
-        branch_name = NULL 
-    WHERE github_issue_number IN (6, 7) 
+    UPDATE tasks
+    SET status = 'NEW',
+        attempt_count = 0,
+        last_error = NULL,
+        current_diff = NULL,
+        branch_name = NULL
+    WHERE github_issue_number IN (6, 7)
     AND github_repo = 'limaronaldo/MultiplAI'
     RETURNING id, github_issue_number, status
   `;
-  
+
   console.log("Reset tasks:", result);
   await sql.end();
 }
